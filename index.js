d9a37c7ee294f4c9d29807b1c4cd8ff2f01d1f94
sos171811als <antlopsou@gmail.com>
2018-03-09 21:18:32 +0000

    corregido index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 
+app.use("/", express.static("/home/ubuntu/workspace/SOS1718-11/public"))
+
 app.get("/hello", (req,res) =>{
     
     res.send("Hello world!");
