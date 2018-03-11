e8fe93d9ddb26386d06cff799d0579dfae228db8
TITANLUX619 <antlopsou@gmail.com>
2018-03-11 11:59:09 +0000

    intercambio linea script start

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ app.get("/hello", (req,res) =>{
 } );
 
 app.listen(process.env.PORT);
+var express = require("express");
+var app = express();
+
+app.use("/", express.static("/home/ubuntu/workspace/SOS1718-11/public"))
+
+app.get("/hello", (req,res) =>{
+    
+    res.send("Hello world!");
+} );
+
+app.listen(process.env.PORT);
