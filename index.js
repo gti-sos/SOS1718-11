ecc1f52b5327f23d2e044d6dfc661148f803188f
TITANLUX619 <antlopsou@gmail.com>
2018-03-11 19:06:00 +0000

    borrado de duplicado

--- index.js
+++ index.js
@@ -9,14 +9,4 @@ app.get("/hello", (req,res) =>{
 } );
 
 app.listen(process.env.PORT);
-var express = require("express");
-var app = express();
 
-app.use("/", express.static("/home/ubuntu/workspace/SOS1718-11/public"))
-
-app.get("/hello", (req,res) =>{
-    
-    res.send("Hello world!");
-} );
-
-app.listen(process.env.PORT);
