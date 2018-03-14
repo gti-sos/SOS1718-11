347030f9f6477d604c74a0d7077aaf0461c313b8
sos171811ldvc <lucianodelvallecano@gmail.com>
2018-03-14 17:44:24 +0000

    chages

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
-var path = require("path")
-var port = (process.env.PORT || 16778)
-var BASE_API_PATH="api/v1"
+var path = require("path");
+var port = (process.env.PORT || 16778);
+var BASE_API_PATH="api/v1";
 
-app.use("/", express.static(path.join(__dirname,"public")))
+app.use("/", express.static(path.join(__dirname,"public")));
 
 app.get("/hello", (req,res) =>{
     
