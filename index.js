d0e3583346f5ac3a696b37317b7440a48cd90c73
sos171811ldvc <lucianodelvallecano@gmail.com>
2018-03-14 17:43:10 +0000

    definido path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var path = require("path")
 var port = (process.env.PORT || 16778)
+var BASE_API_PATH="api/v1"
 
 app.use("/", express.static(path.join(__dirname,"public")))
 
