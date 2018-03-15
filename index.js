0e0af88e31289b87a5b2bc63491e9d909e067f46
sos171811ldvc <lucianodelvallecano@gmail.com>
2018-03-15 23:17:27 +0000

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var path = require("path");
 var port = (process.env.PORT || 16778);
 var BASE_API_PATH="/api/v1";
-var DataStore= require("nedb")
+var DataStore= require("nedb");
 app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname,"public")));
 
