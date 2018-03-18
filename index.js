0ecbdd18d1e9834157335660174206edcb3a95b0
TITANLUX619 <antlopsou@gmail.com>
2018-03-18 15:10:46 +0000

    basketball persistencia

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ var port = (process.env.PORT || 16778);
 var BASE_API_PATH="/api/v1";
 app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname,"public")));
+var DataStore = require("nedb");
+
+
+
+//-------------------basketball-stats-DATABASE-VARIABLES----------------------------//
+var dbBasketball = __dirname+"/basketball-stats.db" 
+var dbbasketball = new DataStore({
+    filename: dbBasketball, 
+    autoload: true
+});
+
+
+
 
 //-------------------------------------------------------------//
 var footballstats= [
@@ -25,49 +38,49 @@ var footballstats= [
     }
     ];
     
-var basketballstats = [
+var initialBasketballstats = [
         { 
         "stadium": "boston", 
         "date": 2018-02-27 , 
-        "mm-first-c" : "49", 
-        "mm-second-c": "15", 
-        "mm-third-c" : "36", 
-        "mm-fourth-c" : "42" 
+        "mm-first-c" : 49, 
+        "mm-second-c": 15, 
+        "mm-third-c" : 36, 
+        "mm-fourth-c" : 42 
         },
         {
         "stadium": "charlote", 
         "date": 2018-03-09 , 
-        "mm-first-c" : "64", 
-        "mm-second-c": "60", 
-        "mm-third-c" : "57", 
-        "mm-fourth-c" : "55"
+        "mm-first-c" : 64, 
+        "mm-second-c": 60, 
+        "mm-third-c" : 57, 
+        "mm-fourth-c" : 55
         },
         {
         "stadium": "miami", 
         "date": 2018-03-09 , 
-        "mm-first-c" : "57", 
-        "mm-second-c": "45", 
-        "mm-third-c" : "45", 
-        "mm-fourth-c" : "66"
+        "mm-first-c" : 57, 
+        "mm-second-c": 45, 
+        "mm-third-c" : 45, 
+        "mm-fourth-c" : 66
         },
         {
         "stadium": "toronto", 
         "date": 2018-03-09 , 
-        "mm-first-c" : "63", 
-        "mm-second-c": "43", 
-        "mm-third-c" : "54", 
-        "mm-fourth-c" : "47"
+        "mm-first-c" : 63, 
+        "mm-second-c": 43, 
+        "mm-third-c" :54, 
+        "mm-fourth-c" : 47
         },
         {
         "stadium": "ocklahoma", 
         "date": 2018-03-09 , 
-        "mm-first-c" : "54", 
-        "mm-second-c": "38", 
-        "mm-third-c" : "68", 
-        "mm-fourth-c" : "47"
+        "mm-first-c" : 54, 
+        "mm-second-c": 38, 
+        "mm-third-c" : 68, 
+        "mm-fourth-c" : 47
         }
     ];
-    
+
     
     
 var baseballstats= [
@@ -109,10 +122,6 @@ var baseballstats= [
     }
     ];
 
-//var db1 = new DataStore({
-  //  filename: dbFileName, 
-//    autoload: true
-//});
 
 
 
@@ -199,6 +208,9 @@ app.put(BASE_API_PATH+"/football-stats/:stadium",(req,res)=>{
 
 
 //-------------------baseball-stats----------------------------//
+
+
+
 app.get(BASE_API_PATH+"/baseball-stats",(req,res)=>{
     console.log(Date(), " - GET /baseballstats")
     res.send(baseballstats);
@@ -274,18 +286,70 @@ app.put(BASE_API_PATH+"/baseball-stats/:stadium",(req,res)=>{
 
 //-------------------basketball-stats----------------------------//
 
+/*
+    dbbasketball.find({}, (err, basketballstats) => {
+        if(err){
+            console.error("Error accessing DB");
+            process.exit(1);
+        }
+        if (basketballstats.length == 0){
+            console.log("Empty DB");
+            dbbasketball.insert(initialBasketballstats);
+        }
+        else{
+            console.log("DB initialized with " + basketballstats.length + " stats");
+        }
+    });
+*/
+
+
+app.get(BASE_API_PATH+"/basketball-stats/loadInitialBasketballstats",(req,res)=> {
+        
+        dbbasketball.insert(initialBasketballstats, function (err, newDoc) {
+            if(err){
+                console.error("Error accesing DB");
+                res.sendStatus(500);
+                return;
+            };
+        
+        });
+        res.sendStatus(200);
+        console.log("Insertados  " + initialBasketballstats.length + " stats");
+});
+
+app.get(BASE_API_PATH+"/basketball-help",(req,res)=>{
+    res.redirect("https://documenter.getpostman.com/view/3936462/collection/RVnYDKSF")
+});
+
 
 app.get(BASE_API_PATH+"/basketball-stats",(req,res)=>{
-    console.log(Date() + " - GET /basketball-stats");
-    res.send(basketballstats);
+
+    dbbasketball.find({},(err,basketballstats)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+            return;
+        }
+        console.log(Date() + " - GET /basketball-stats");
+        res.send(basketballstats);
+    });
 });
+
 
 app.post(BASE_API_PATH+"/basketball-stats",(req,res)=>{
     console.log(Date() + " - POST /basketball-stats");
-    var stat = req.body;
-    basketballstats.push(stat);
-    res.sendStatus(201);
+    var basketballstat = req.body;
+    
+    dbbasketball.insert(basketballstat, function (err, newDoc) {
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+            return;
+        };
+        res.sendStatus(200);
+    });
 });
+
 
 app.put(BASE_API_PATH+"/basketball-stats",(req,res)=>{
     console.log(Date() + " - PUT /basketball-stats");
@@ -293,31 +357,60 @@ app.put(BASE_API_PATH+"/basketball-stats",(req,res)=>{
 });
 
 app.delete(BASE_API_PATH+"/basketball-stats",(req,res)=>{
+    
     console.log(Date() + " - DELETE /basketball-stats");
-    basketballstats = [];
-    res.sendStatus(200);
+
+    dbbasketball.remove({}, { multi: true }, function (err, numRemoved) {
+            if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+            return;
+        };
+        console.log("DELETE "+numRemoved);
+        res.sendStatus(200);
+    });
 });
 
 
 app.get(BASE_API_PATH+"/basketball-stats/:stadium",(req,res)=>{
     var stadium = req.params.stadium;
-    console.log(Date() + " - GET /basketball-stats/"+stadium);
-    
-    res.send(basketballstats.filter((c)=>{
-        return (c.stadium == stadium);
-    })[0]);
+
+    dbbasketball.find({"stadium":stadium},(err,basketballstats)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+            return;
+        }else if(basketballstats.length==0){
+            res.sendStatus(404);
+            return;
+        };
+        console.log(Date() + " - GET /basketball-stats "+ stadium );
+        res.send(basketballstats);
+    });
 });
+
 
 app.delete(BASE_API_PATH+"/basketball-stats/:stadium",(req,res)=>{
     var stadium = req.params.stadium;
+    
     console.log(Date() + " - DELETE /basketball-stats/"+stadium);
-    
-    basketballstats = basketballstats.filter((c)=>{
-        return (c.stadium != stadium);
+
+    dbbasketball.remove({ "stadium": stadium }, function (err, numRemoved) {
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+            return;
+        }else if(numRemoved==0){
+            res.sendStatus(404);
+            return;
+        };
+        console.log("DELETE "+numRemoved);
+        res.sendStatus(200);
+  
     });
-    
-    res.sendStatus(200);
 });
+
+
 
 app.post(BASE_API_PATH+"/basketball-stats/:stadium",(req,res)=>{
     var stadium = req.params.stadium;
@@ -325,27 +418,76 @@ app.post(BASE_API_PATH+"/basketball-stats/:stadium",(req,res)=>{
     res.sendStatus(405);
 });
 
+
 app.put(BASE_API_PATH+"/basketball-stats/:stadium",(req,res)=>{
+    
     var stadium = req.params.stadium;
-    var stat = req.body;
+    var basketballstat = req.body;
     
     console.log(Date() + " - PUT /basketball-stats/"+stadium);
+    dbbasketball.update({"stadium": stadium}, basketballstat,(err,numUpdated)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+            return;
+        }else if(numUpdated==0){
+            res.sendStatus(404);
+            return;
+        };
+        console.log("UPDATED "+numUpdated);
+        res.sendStatus(200);
+    });
+});    
+
+
+/*
+app.get(BASE_API_PATH+"/basketball-stats",(req,res)=>{
+    console.log(Date() + " - GET /basketball-stats");
+    res.send(basketballstats);
+});
+
+
+app.get(BASE_API_PATH+"/basketball-stats/:stadium",(req,res)=>{
+    var stadium = req.params.stadium;
+   console.log(Date() + " - GET /basketball-stats/"+stadium);
     
+    res.send(basketballstats.filter((c)=>{
+        return (c.stadium == stadium);
+    })[0]);
+});
+
+
+app.post(BASE_API_PATH+"/basketball-stats",(req,res)=>{
+    basketballstats.push(stat);
+    res.sendStatus(201);
+});
+
+
+app.put(BASE_API_PATH+"/basketball-stats/:stadium",(req,res)=>{    
     if(stadium != stat.stadium){
-        res.sendStatus(409);
+         res.sendStatus(409);
         console.warn(Date()+" - Hacking attempt!");
         return;
-    }
-    
+
     basketballstats = basketballstats.map((c)=>{
-        if(c.stadium == stat.stadium)
-            return stat;
-        else
-            return c;
+      if(c.stadium == stat.stadium)
+      return stat;
+      else
+         return c;
     });
     
     res.sendStatus(200);
 });
+
+
+app.delete(BASE_API_PATH+"/basketball-stats",(req,res)=>{
+    console.log(Date() + " - DELETE /basketball-stats");
+    basketballstats = [];
+    res.sendStatus(200);
+});
+*/
+
+
 
 //--------------------------------------------------------------//
 app.listen(port,()=>{
@@ -353,4 +495,3 @@ app.listen(port,()=>{
 }).on("error",(e)=>{
     console.log("Server NOT READY:"+e);
 });
-
