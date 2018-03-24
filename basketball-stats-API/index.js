9d2f3ded078992dd72a8358ffa55113571ee0645
TITANLUX619 <antlopsou@gmail.com>
2018-03-24 18:07:25 +0000

    pulido busquedas y paginacion

--- basketball-stats-API/index.js
+++ basketball-stats-API/index.js
@@ -21,12 +21,28 @@ var initialBasketballstats = [{
         "mm-fourth-c": 55
     },
     {
+        "stadium": "boston",
+        "date": "2018-03-09",
+        "mm-first-c": 64,
+        "mm-second-c": 60,
+        "mm-third-c": 57,
+        "mm-fourth-c": 55
+    },
+    {
         "stadium": "miami",
         "date": "2018-03-09",
         "mm-first-c": 57,
         "mm-second-c": 45,
         "mm-third-c": 45,
         "mm-fourth-c": 66
+    },
+        {
+        "stadium": "boston",
+        "date": "2018-04-09",
+        "mm-first-c": 64,
+        "mm-second-c": 60,
+        "mm-third-c": 57,
+        "mm-fourth-c": 55
     },
     {
         "stadium": "toronto",
@@ -42,6 +58,14 @@ var initialBasketballstats = [{
         "mm-first-c": 54,
         "mm-second-c": 38,
         "mm-third-c": 68,
+        "mm-fourth-c": 47
+    },
+    {
+        "stadium": "toronto",
+        "date": "2018-05-09",
+        "mm-first-c": 63,
+        "mm-second-c": 43,
+        "mm-third-c": 54,
         "mm-fourth-c": 47
     }
 ];
@@ -60,7 +84,7 @@ basketballstatsAPI.register = function(app, dbbasketballstats, checkApiKey) {
     var buscador = function(base, aux_set, param_from, param_to, param_stadium, param_date) {
 
         console.log("Búsqueda con parametros: from = " + param_from + " ,to = " + param_to + ", stadium = " + param_stadium + ", date = " + param_date + ".")
-
+        
         var f = new Date(param_from);
         var t = new Date(param_to);
 
@@ -68,43 +92,70 @@ basketballstatsAPI.register = function(app, dbbasketballstats, checkApiKey) {
 
             var date = new Date(base[j].date);
             var stadium = base[j].stadium;
-            if (param_from != undefined && param_to != undefined) {
+            
+            // FROM + TO + STADIUM
+            if (param_from != undefined && param_to != undefined && param_stadium != undefined && param_date == undefined) {
+                
                 if (f <= date && t >= date && param_stadium == stadium) {
                         aux_set.push(base[j]);
                 }
-                else if (param_to >= date && param_from <= date) {
-                    aux_set.push(base[j]);
-                }
-            }
-            else if (param_from != undefined && param_to == undefined) {
+                
+            // FROM + STADIUM
+            }else if (param_from != undefined && param_to == undefined && param_stadium != undefined && param_date == undefined) {
+                
                 if (f <= date && param_stadium == stadium) {
                         aux_set.push(base[j]);
                 }
-                else if (param_from <= date) {
-                    aux_set.push(base[j]);
-                }
-            }
-            else if (param_from == undefined && param_to != undefined) {
+            
+            // TO + STADIUM
+            }else if (param_from == undefined && param_to != undefined && param_stadium != undefined && param_date == undefined) {
+               
                 if (t >= date && param_stadium == stadium) {
                         aux_set.push(base[j]);
                 }
-                else if (t >= date) {
-                    aux_set.push(base[j]);
+                
+            //FROM + TO
+            }else if (param_from != undefined && param_to != undefined && param_stadium == undefined && param_date == undefined) {
+                
+                if (f <= date && t >= date) {
+                        aux_set.push(base[j]);
                 }
-            }else if(param_from == undefined && param_to == undefined && param_stadium == stadium){
-                aux_set.push(base[j]);
-            }
             
-            if (date == param_date && stadium == param_stadium) {
-                aux_set.push(base[j]);
-            }
+            // FROM
+            }else if (param_from != undefined && param_to == undefined && param_stadium == undefined && param_date == undefined) {
+               
+                if (f <= date) {
+                        aux_set.push(base[j]);
+                }
+            
+            // TO
+            }else if (param_from == undefined && param_to != undefined && param_stadium == undefined && param_date == undefined) {
+               
+                if (t >= date) {
+                        aux_set.push(base[j]);
+                }    
+            // STADIUM + DATE    
+            }else if (param_from == undefined && param_to == undefined && param_stadium != undefined && param_date != undefined) {
+                if (param_stadium == stadium && param_date == base[j].date) {
+                        aux_set.push(base[j]);
+                } 
+            
+            // STADIUM   
+            }else if(param_from == undefined && param_to == undefined && param_stadium != undefined && param_date == undefined){
+                
+                if (param_stadium == stadium) {
+                        aux_set.push(base[j]);
+                }
+            
+            // DATE    
+            }else if (param_from == undefined && param_to == undefined && param_stadium == undefined && param_date != undefined) {
 
-            if (date == param_date && param_stadium == undefined) {
-                aux_set.push(base[j]);
+                if (param_date == base[j].date) {
+                        aux_set.push(base[j]);
+                }                
             }
 
         }
-
 
         return aux_set;
 
@@ -166,16 +217,17 @@ basketballstatsAPI.register = function(app, dbbasketballstats, checkApiKey) {
                     if (basketballstats.length === 0) {
                         res.sendStatus(404);
                     }
-                    console.log("INFO: Sending basketballstats: " + JSON.stringify(basketballstats, 2, null));
+//                    console.log("INFO: Sending basketballstats: " + JSON.stringify(basketballstats, 2, null));
 
                     if (from || to || stadium || date) {
 
                         aux = buscador(basketballstats, aux, from, to, stadium, date);
                         if (aux.length > 0) {
                             aux2 = aux.slice(offset, offset + limit);
-                            console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(aux, 2, null));
+/*                            console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(aux, 2, null));
                             console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(basketballstats, 2, null));
-                            console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(aux2, 2, null));
+                          console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(aux2, 2, null));
+*/
                             res.send(aux2);
 
                         }
@@ -198,15 +250,14 @@ basketballstatsAPI.register = function(app, dbbasketballstats, checkApiKey) {
                     res.sendStatus(500); // internal server error
                 }
                 else {
-                    if (basketballstats.length === 0) {
+                    if (basketballstats.length == 0) {
                         res.sendStatus(204);
                         return;
                     }
-                    console.log("INFO: Sending results: " + JSON.stringify(basketballstats, 2, null));
-                    if (from && to || stadium || date) {
+//                    console.log("INFO: Sending results: " + JSON.stringify(basketballstats, 2, null));
+                    if (from || to || stadium || date) {
                         aux = buscador(basketballstats, aux, from, to, stadium, date);
                         if (aux.length > 0) {
-                            console.log("busqueda");
                             res.send(aux);
                         }
                         else {
