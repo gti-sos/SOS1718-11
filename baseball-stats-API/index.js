f11470c16900b2622f253d8c9298559ff164e9ec
sos171811mls <mar.soult.91@gmail.com>
2018-04-02 10:32:20 +0000

    correcion feedback

--- baseball-stats-API/index.js
+++ baseball-stats-API/index.js
@@ -1,4 +1,5 @@
 var BASE_API_PATH = "/api/v1";
+var BASE_API_PATH_secure = "/api/v1/secure";
 var baseballstatsAPI = {};
 module.exports = baseballstatsAPI;
 
@@ -183,7 +184,7 @@ baseballstatsAPI.register = function(app, dbbaseballstats, checkApiKey) {
 
     //-------------------------------METODOS----------------------------------------//
 
-    app.get(BASE_API_PATH + "/baseball-help", (req, res) => {
+    app.get(BASE_API_PATH + "/baseball-stats/docs", (req, res) => {
         res.redirect("https://documenter.getpostman.com/view/3883703/collection/RVnYDKMz");
 
     });
@@ -191,7 +192,6 @@ baseballstatsAPI.register = function(app, dbbaseballstats, checkApiKey) {
 
     //Inicializa base de datos vacia
     app.get(BASE_API_PATH + "/baseball-stats/loadInitialData", (req, res) => {
-        if (!checkApiKey(req, res)) return;
         dbbaseballstats.insert(initialbaseballstats, function(err, newDoc) {
             if (err) {
                 console.log("Error accesing ");
@@ -211,7 +211,6 @@ baseballstatsAPI.register = function(app, dbbaseballstats, checkApiKey) {
 
 
     app.post(BASE_API_PATH + "/baseball-stats", (req, res) => {
-        if (!checkApiKey(req, res)) return;
         console.log(Date() + " - POST /baseball-stats");
         var baseballstat = req.body;
 
@@ -362,8 +361,8 @@ baseballstatsAPI.register = function(app, dbbaseballstats, checkApiKey) {
         console.log(Date() + " - PUT /baseball-stats/" + stadium + date);
 
         if (stadium != baseballstat.stadium || date != baseballstat.date) {
-            console.log("Not found");
-            res.sendStatus(404);
+            console.log("Bad request");
+            res.sendStatus(400);
             return;
 
         }
@@ -389,7 +388,6 @@ baseballstatsAPI.register = function(app, dbbaseballstats, checkApiKey) {
 
 
     app.get(BASE_API_PATH + "/baseball-stats/:stadium/:date", (req, res) => {
-        if (!checkApiKey(req, res)) return;
         var stadium = req.params.stadium;
         var date = req.params.date;
 
@@ -411,7 +409,6 @@ baseballstatsAPI.register = function(app, dbbaseballstats, checkApiKey) {
 
 
     app.get(BASE_API_PATH + "/baseball-stats/:dato", (req, res) => {
-        if (!checkApiKey(req, res)) return;
 
         var limit = parseInt(req.query.limit);
         var offset = parseInt(req.query.offset);
@@ -493,7 +490,423 @@ baseballstatsAPI.register = function(app, dbbaseballstats, checkApiKey) {
     // GET Collection (WITH SEARCH)
 
     app.get(BASE_API_PATH + "/baseball-stats", function(request, response) {
-        if (!checkApiKey(request, response)) return;
+        console.log("INFO: New GET request to /baseball-stats ");
+
+        /*PRUEBA DE BUSQUEDA */
+        var limit = parseInt(request.query.limit);
+        var offset = parseInt(request.query.offset);
+        var from = request.query.from;
+        var to = request.query.to;
+        var stadium = request.query.stadium;
+        var date = request.query.date;
+        var hit = request.query.hit;
+        var run = request.query.run;
+        var error = request.query.error;
+        var aux = [];
+        var aux2 = [];
+        var aux3 = [];
+
+
+        if (limit || offset >= 0) {
+            dbbaseballstats.find({}).skip(offset).limit(limit).toArray(function(err, baseballstats) {
+                if (err) {
+                    console.error('WARNING: Error getting data from DB');
+                    response.sendStatus(500); // internal server error
+                    return;
+                }
+                else {
+                    if (baseballstats.length === 0) {
+                        response.sendStatus(404); //No content
+                        return;
+                    }
+                    console.log("INFO: Sending countries:: " + JSON.stringify(baseballstats, 2, null));
+                    if (from || to || stadium || date || hit || run || error) {
+
+                        aux = buscador(baseballstats, aux, from, to, stadium, date, hit, run, error);
+                        if (aux.length > 0) {
+                            aux2 = aux.slice(offset, offset + limit);
+                            //console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(aux, 2, null));
+                            //console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(baseballstats, 2, null));
+                            //console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(aux2, 2, null));
+                            response.send(aux2);
+                        }
+                        else {
+
+                            response.send(aux3); // No content 
+                            return;
+                        }
+                    }
+                    else {
+                        response.send(baseballstats);
+                    }
+                }
+            });
+
+        }
+        else {
+
+            dbbaseballstats.find({}).toArray(function(err, baseballstats) {
+                if (err) {
+                    console.error('ERROR from database');
+                    response.sendStatus(500); // internal server error
+                }
+                else {
+                    if (baseballstats.length === 0) {
+
+                        response.send(baseballstats);
+                        return;
+                    }
+                    //console.log("INFO: Sending baseball-stats: " + JSON.stringify(baseballstats, 2, null));
+                    if (from || to || stadium || date || hit || run || error) {
+                        aux = buscador(baseballstats, aux, from, to, stadium, date, hit, run, error);
+                        if (aux.length > 0) {
+                            response.send(aux);
+                        }
+                        else {
+                            response.sendStatus(404); //No content
+                            return;
+                        }
+                    }
+                    else {
+                        response.send(baseballstats);
+                    }
+                }
+            });
+        }
+
+    });
+
+
+
+
+
+//----------------------------------------------SECURE---------------------------------------------------
+
+
+    //Inicializa base de datos vacia
+    app.get(BASE_API_PATH_secure + "/baseball-stats/loadInitialData", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+        dbbaseballstats.insert(initialbaseballstats, function(err, newDoc) {
+            if (err) {
+                console.log("Error accesing ");
+                process.exit(1);
+                return;
+            }
+            else {
+                console.log("Created");
+                res.sendStatus(201);
+                console.log("DB initialized with: " + initialbaseballstats.length + " partidos");
+            }
+        });
+
+    });
+
+
+
+
+    app.post(BASE_API_PATH_secure + "/baseball-stats", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+        console.log(Date() + " - POST /baseball-stats");
+        var baseballstat = req.body;
+
+
+        if (!baseballstat.stadium || !baseballstat.date || !baseballstat.hit || !baseballstat.run || !baseballstat.error || Object.keys(baseballstat).length != 5) {
+            res.sendStatus(400);
+            return;
+        }
+        else {
+            dbbaseballstats.find({ "stadium": baseballstat.stadium, "date": baseballstat.date }).toArray((err, baseballstats) => {
+                if (err) {
+                    console.error("Error accesing DB");
+                    res.sendStatus(500);
+                    return;
+                }
+                else if (baseballstats.length != 0) {
+                    res.sendStatus(409);
+                    return;
+                }
+                else {
+                    dbbaseballstats.insert(baseballstat, function(err, newDoc) {
+                        if (err) {
+                            console.error("Error accesing DB");
+                            res.sendStatus(500);
+                            return;
+                        }
+                        else {
+                            res.sendStatus(201);
+                            console.log("INSERTED 1 element");
+                        }
+
+                    });
+                }
+
+            });
+        }
+    });
+
+
+
+    //PUT a ruta base (Error)
+    app.put(BASE_API_PATH_secure + "/baseball-stats", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+        console.log(Date() + " - PUT /baseball-stats");
+        console.log("Method not allowed");
+        res.sendStatus(405);
+    });
+
+
+    //DELETE a ruta base
+    app.delete(BASE_API_PATH_secure + "/baseball-stats", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+        console.log(Date() + " - DELETE /baseball-stats");
+
+        dbbaseballstats.remove({}, { multi: true }, (err, numRemoved) => {
+            if (err) {
+                console.log("Error accesing DB");
+                res.sendStatus(500);
+            }
+            else if (numRemoved.result.n == 0) {
+                console.log("Not found");
+                res.sendStatus(404);
+            }
+            else {
+                console.log("Removed: " + numRemoved.result.n);
+                res.sendStatus(200);
+
+            }
+        });
+    });
+
+
+
+    //DELETE a un conjunto de recursos concreto
+    app.delete(BASE_API_PATH_secure + "/baseball-stats/:stadium", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+        var stadium = req.params.stadium;
+
+        console.log(Date() + " - DELETE /baseball-stats/" + stadium);
+
+        dbbaseballstats.remove({ "stadium": stadium }, { multi: true }, (err, numRemoved) => {
+            if (err) {
+                console.log("Error accesing DB");
+                res.sendStatus(500);
+                return;
+            }
+            else if (numRemoved.result.n == 0) {
+                console.log("Not found");
+                res.sendStatus(404);
+            }
+            else {
+                console.log("Removed: " + numRemoved.result.n);
+                res.sendStatus(200);
+            }
+        });
+    });
+
+
+    //DELETE a un recurso concreto
+    app.delete(BASE_API_PATH_secure + "/baseball-stats/:stadium/:date", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+        var stadium = req.params.stadium;
+        var date = req.params.date;
+        console.log(Date() + " - DELETE /baseball-stats/" + stadium + date);
+
+        dbbaseballstats.remove({ "stadium": stadium, "date": date }, (err, numRemoved) => {
+            if (err) {
+                console.log("Error remove");
+                res.sendStatus(500);
+                return;
+            }
+            else if (numRemoved.result.n == 0) {
+                console.log("Not found");
+
+                res.sendStatus(404);
+            }
+            else {
+                console.log("Removed: " + numRemoved.result.n);
+                res.sendStatus(200);
+            }
+        });
+    });
+
+    //POST a un recurso concreto (Error)
+    app.post(BASE_API_PATH_secure + "/baseball-stats/:stadium", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+        var stadium = req.params.stadium;
+        console.log(Date() + " - POST /baseball-stats/" + stadium);
+        console.log("Method not allowed");
+
+        res.sendStatus(405);
+        return;
+    });
+
+    //PUT a conjunto recursos (Error)
+    app.put(BASE_API_PATH_secure + "/baseball-stats/:stadium", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+        var stadium = req.params.stadium;
+        console.log(Date() + " - PUT /baseball-stats/" + stadium);
+        console.log("Error 405");
+        res.sendStatus(405);
+        return;
+    });
+
+
+    //PUT a un recurso concreto
+    app.put(BASE_API_PATH_secure + "/baseball-stats/:stadium/:date", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+        var stadium = req.params.stadium;
+        var date = req.params.date;
+        var baseballstat = req.body;
+
+
+
+        console.log(Date() + " - PUT /baseball-stats/" + stadium + date);
+
+        if (stadium != baseballstat.stadium || date != baseballstat.date) {
+            console.log("Bad request");
+            res.sendStatus(400);
+            return;
+
+        }
+
+        dbbaseballstats.update({ "stadium": stadium, "date": date }, baseballstat, (err, numUpdated) => {
+            if (err) {
+                console.log("Error accesing data base");
+                res.sendStatus(500);
+                return;
+            }
+            else if (numUpdated.result.n == 0) {
+                console.log("Not found");
+                res.sendStatus(404);
+                return;
+            }
+            else {
+                console.log("Updated: " + numUpdated.result.n);
+                res.sendStatus(200);
+            }
+        });
+
+    });
+
+
+    app.get(BASE_API_PATH_secure + "/baseball-stats/:stadium/:date", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+        var stadium = req.params.stadium;
+        var date = req.params.date;
+
+
+        dbbaseballstats.find({ "stadium": stadium, "date": date }).toArray((err, baseballstats) => {
+            if (err) {
+                console.error("Error accesing DB");
+                res.sendStatus(500);
+                return;
+            }
+            else if (baseballstats.length == 0) {
+                res.sendStatus(404);
+                return;
+            }
+            console.log(Date(), "- GET /baseball-stats/" + stadium + "/" + date);
+            res.send(baseballstats[0]);
+        });
+    });
+
+
+    app.get(BASE_API_PATH_secure + "/baseball-stats/:dato", (req, res) => {
+                if (!checkApiKey(req, res)) return;
+
+
+        var limit = parseInt(req.query.limit);
+        var offset = parseInt(req.query.offset);
+        var from = req.query.fromDate;
+        var to = req.query.toDate;
+        var stadium = req.query.stadium;
+        var date = req.query.date;
+        var hit = req.query.hit;
+        var run = req.query.run;
+        var error = req.query.error;
+
+        var aux = [];
+        var aux2 = [];
+        var dato = req.params.dato;
+
+        if (limit || offset >= 0) {
+            dbbaseballstats.find({ $or: [{ "stadium": dato }, { "date": dato }] }).skip(offset).limit(limit).toArray(function(err, baseballstats) {
+
+                if (err) {
+                    console.error('WARNING: Error getting data from DB');
+                    res.sendStatus(500); // internal server error
+                }
+                else {
+                    if (baseballstats.length === 0) {
+                        res.sendStatus(404);
+                    }
+
+                    if (from || to || date || hit || run || error) {
+
+                        aux = buscador(baseballstats, aux, from, to, stadium, date, hit, run, error);
+                        if (aux.length > 0) {
+                            aux2 = aux.slice(offset, offset + limit);
+                            res.send(aux2);
+
+                        }
+                        else {
+                            res.sendStatus(404); // No content 
+                        }
+                    }
+                    else {
+                        res.send(baseballstats);
+                    }
+                }
+            });
+
+        }
+        else {
+
+            dbbaseballstats.find({ $or: [{ "stadium": dato }, { "date": dato }] }).toArray((err, baseballstats) => {
+                if (err) {
+                    console.error("Error accesing DB");
+                    res.sendStatus(500);
+
+                }
+                else {
+                    if (baseballstats.length == 0) {
+                        res.sendStatus(404);
+                        return;
+                    }
+                    if (from || to || date || hit || run || error) {
+                        aux = buscador(baseballstats, aux, from, to, stadium, date, hit, run, error);
+                        if (aux.length > 0) {
+                            res.send(aux);
+                        }
+                        else {
+                            res.sendStatus(404); //No content
+                        }
+                    }
+                    else {
+                        console.log(Date() + " - GET /baseball-stats/" + dato);
+                        res.send(baseballstats);
+                    }
+                }
+            });
+
+        }
+    });
+
+    // GET Collection (WITH SEARCH)
+
+    app.get(BASE_API_PATH_secure + "/baseball-stats", function(request, response) {
+                if (!checkApiKey(request, response)) return;
+
         console.log("INFO: New GET request to /baseball-stats ");
 
         /*PRUEBA DE BUSQUEDA */
