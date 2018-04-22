6c01cfd6215dbc49c15df136f47ac9a6857b44f4
sos171811ldvc <lucianodelvallecano@gmail.com>
2018-04-22 20:38:34 +0000

    ee

--- public/baseballStats/listCtrl.js
+++ public/baseballStats/listCtrl.js
@@ -11,7 +11,7 @@
              $http.post(api, $scope.newBaseballStat).then(function(response) {
                  $scope.status = "Status: " + response.status;
                  console.log(JSON.stringify(response, null, 2));
-                // getBaseballStats();
+                 getBaseballStats();
 
              });
          };
@@ -20,7 +20,7 @@
              console.log("Stat to be deleted");
              $http.delete(api + "/" + stadium + "/" + date).then(function(response) {
                  $scope.status = "Status: " + response.status;
-               //  getBaseballStats();
+                 getBaseballStats();
              });
          };
 
