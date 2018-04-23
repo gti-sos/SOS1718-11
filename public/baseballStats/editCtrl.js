c4bbacb1970fc4e5362c8da8907d654c079fb22b
sos171811mls <mar.soult.91@gmail.com>
2018-04-23 01:53:46 +0000

    baseball nocturno

--- public/baseballStats/editCtrl.js
+++ public/baseballStats/editCtrl.js
@@ -1,26 +1,31 @@
  /*global angular*/
  angular
- .module("StatsManagerApp")
- .controller("EditBaseballStatsCtrl", ["$scope","$http", "$routeParams", function($scope, $http, $routeParams){
-                
-                var editURL="/api/v2/baseball-stats/" + $routeParams.stadium + "/" + $routeParams.date;
-                console.log("initialized EditBaseballStatsCtrl");
-                console.log(editURL);
-                
-                 $http.get(editURL).then(function (response){
-                        $scope.updatedBaseballStat = response.data;
-                 });
-                 
-                 
-                 $scope.updateBaseballStat = function(){
-                     console.log($scope.updatedBaseballStat);
-                     console.log($scope.updatedBaseballStat);
-                    $http.put(editURL, $scope.updatedBaseballStat).then(function (response){
-                    $scope.status = "Status: " + response.status;
-                     
-                 });
-                 
-             
-                }
- }]);
+     .module("StatsManagerApp")
+     .controller("EditBaseballStatsCtrl", ["$scope", "$http", "$routeParams", function($scope, $http, $routeParams) {
+
+         var editURL = "/api/v2/baseball-stats/" + $routeParams.stadium + "/" + $routeParams.date;
+         console.log("initialized EditBaseballStatsCtrl");
+         console.log(editURL);
+
+         $http.get(editURL).then(function(response) {
+             $scope.updatedBaseballStat = response.data;
+         });
+
+
+         $scope.updateBaseballStat = function() {
+             var updatedStatAux = {};
+             updatedStatAux.stadium = $routeParams.stadium;
+             updatedStatAux.date = $routeParams.date;
+             updatedStatAux.hit = parseInt($scope.updatedBaseballStat.hit);
+             updatedStatAux.run = parseInt($scope.updatedBaseballStat.run);
+             updatedStatAux.error = parseInt($scope.updatedBaseballStat.error);
+             console.log($scope.updatedBaseballStat);
+             $http.put(editURL, updatedStatAux).then(function(response) {
+                 $scope.status = "Status: " + response.status;
+
+             });
+
+
+         }
+     }]);
  
