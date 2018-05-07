bfc9e000811b0e42114ae24ff146f49282a0470c
sos171811ldvc <lucianodelvallecano@gmail.com>
2018-05-07 08:28:01 +0000

    changes

--- public/footballStats/d3Ctrl.js
+++ public/footballStats/d3Ctrl.js
@@ -7,91 +7,92 @@ angular.module("StatsManagerApp").controller("D3Ctrl", ["$scope","$http","$rootS
     var api ="/api/v2/secure/football-stats";
      if(!$rootScope.apikey) $rootScope.apikey = "scraping";
     
-     var datos = [[]];
+     var datos =[];
+     // anychart.Clear(document.getElementById("graphfootball"));
     $http.get(api + "?apikey=" + $rootScope.apikey).then(function (response){
-      console.log(datos);
-     
-    var chart = AmCharts.makeChart("chartdiv", {
-  "type": "serial",
-  "theme": "light",
-  "marginRight": 70,
-  "dataProvider": [{
-    "country": "USA",
-    "visits": 3025,
-    "color": "#FF0F00"
-  }, {
-    "country": "China",
-    "visits": 1882,
-    "color": "#FF6600"
-  }, {
-    "country": "Japan",
-    "visits": 1809,
-    "color": "#FF9E01"
-  }, {
-    "country": "Germany",
-    "visits": 1322,
-    "color": "#FCD202"
-  }, {
-    "country": "UK",
-    "visits": 1122,
-    "color": "#F8FF01"
-  }, {
-    "country": "France",
-    "visits": 1114,
-    "color": "#B0DE09"
-  }, {
-    "country": "India",
-    "visits": 984,
-    "color": "#04D215"
-  }, {
-    "country": "Spain",
-    "visits": 711,
-    "color": "#0D8ECF"
-  }, {
-    "country": "Netherlands",
-    "visits": 665,
-    "color": "#0D52D1"
-  }, {
-    "country": "Russia",
-    "visits": 580,
-    "color": "#2A0CD0"
-  }, {
-    "country": "South Korea",
-    "visits": 443,
-    "color": "#8A0CCF"
-  }, {
-    "country": "Canada",
-    "visits": 441,
-    "color": "#CD0D74"
-  }],
-  "valueAxes": [{
-    "axisAlpha": 0,
-    "position": "left",
-    "title": "Visitors from country"
-  }],
-  "startDuration": 1,
-  "graphs": [{
-    "balloonText": "<b>[[category]]: [[value]]</b>",
-    "fillColorsField": "color",
-    "fillAlphas": 0.9,
-    "lineAlpha": 0.2,
-    "type": "column",
-    "valueField": "visits"
-  }],
-  "chartCursor": {
-    "categoryBalloonEnabled": false,
-    "cursorAlpha": 0,
-    "zoomable": false
-  },
-  "categoryField": "country",
-  "categoryAxis": {
-    "gridPosition": "start",
-    "labelRotation": 45
-  },
-  "export": {
-    "enabled": true
-  }
+      
+       var stadium = response.data.map(function(d) {
 
-});
+            return d['stadium'];
+
+        });
+        var goal = response.data.map(function(d) {
+
+                
+
+            return d['goal'];
+
+        });
+         for (var i = 0; i < response.data.length; i++) {
+            datos.push([stadium[i], parseInt(goal[i])]);
+        }
+        
+        
+      
+     
+anychart.onDocumentReady(function() {
+  // create bar chart
+  var chart = anychart.bar();
+
+  // turn on chart animation
+  chart.animation(true)
+    // set chart title text settings
+    .title('World\'s largest cities');
+
+  chart.credits()
+    .enabled(true)
+    .url('https://en.wikipedia.org/wiki/List_of_cities_proper_by_population')
+    .text('Data source: https://en.wikipedia.org/wiki/List_of_cities_proper_by_population')
+    .logoSrc('https://en.wikipedia.org/static/favicon/wikipedia.ico');
+
+  // create stick series with passed data
+  chart.stick(getData());
+
+  // set scale minimum
+  chart.yScale().minimum(0).ticks().interval(5);
+  // tooltips position and interactivity settings
+  chart.tooltip()
+    .positionMode('point')
+    // set series tooltip settings
+    .format(function() {
+      return 'Goals: ' +
+        anychart.format.number(this.value, {
+          groupsSeparator: ' '
+        }) 
     });
+
+  chart.interactivity().hoverMode('by-x');
+  // axes titles
+  chart.yAxis().title('Population (millions)');
+  // set container id for the chart
+  chart.container('graphfootball');
+  // initiate chart drawing
+  chart.draw();
+});
+
+function getData() {
+  return datos;
+  /*return [
+    ['Shanghai', 24.2],
+    ['Karachi', 23.5],
+    ['Beijing', 21.5],
+    ['Delhi', 16.7],
+    ['Lagos', 16.0],
+    ['Tianjin', 15.2],
+    ['Istanbul', 14.1],
+    ['Tokyo', 13.5],
+    ['Guangzhou', 13],
+    ['Mumbai', 12.4],
+    ['Moscow', 12.1],
+    ['S?o Paulo', 11.8],
+    ['Shenzhen', 10.4],
+    ['Jakarta', 10.1],
+    ['Lahore', 10],
+    ['Seoul', 9.9],
+    ['Wuhan', 9.7],
+    ['Kinshasa', 9.7],
+    ['Cairo', 9.2],
+    ['Mexico City', 8.8]
+  ];*/
+}    });
 }]);
