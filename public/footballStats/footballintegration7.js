70389df02e06c46ff189f8c2703a8dd19c936abc
sos171811ldvc <lucianodelvallecano@gmail.com>
2018-05-31 07:45:28 +0000

    a

---
+++ public/footballStats/footballintegration7.js
@@ -0,0 +1,181 @@
+ /*global angular*/
+ /* global $ */
+ /* global anychart*/
+ /*global zingchart*/
+
+ angular.module("StatsManagerApp").controller("FootballIntegration7Ctrl", ["$scope", "$http", "$rootScope", function($scope, $http, $rootScope) {
+  console.log("List Football Ctrl initialized!");
+
+  if (!$rootScope.apikey) $rootScope.apikey = "scraping";
+  var api = "/api/v2/secure/football-stats";
+  var datos;
+  var nombrepiloto = [];
+  var cadena = [];
+  var datosbolo = [];
+  var sumascore=0;
+  var sumagoles=0.0;
+  var sumacornes=0.0;
+  var sumafaltas=0.0;
+  var uu="500";
+  var score="";
+  
+
+
+
+  $http.get(api + '?apikey=' + $rootScope.apikey).then(function(response) {
+
+                    var footballData = response.data;
+                    console.log(footballData);
+                    
+
+                    $http.get("https://sos1718-10.herokuapp.com/api/v1/motogp-stats").then(function(response) {
+
+                            var bolo = response.data;
+                            
+                            for (var i=0;i<footballData.length;i++){
+                               sumagoles+= footballData[i].goal;
+                               sumacornes+= footballData[i].corner;
+                               sumafaltas+= footballData[i].fault;
+                               
+                                
+                            }
+                            for (var i=0;i<bolo.length;i++){
+                               sumascore+= bolo[i].score;
+                                
+                            }
+                            
+                            console.log("Numero total de goles" + sumagoles);
+                            console.log("Numero total de corner" + sumafaltas);
+                            console.log("Numero total de faltas" + sumafaltas);
+                            console.log("Numero total de score" + sumascore);
+
+
+
+
+ 
+score=JSON.stringify(sumascore);
+
+
+
+
+    });
+
+
+
+
+   });
+   
+   
+score=JSON.parse(sumascore)
+zingchart.THEME="classic";
+var myConfig = 
+        {
+    "graphset": [
+        {
+            "type": "hfunnel",
+            "background-color": "#fff",
+            "background-color-2": "#f1f1f1",
+            "scale-x": {
+                "values": [
+                    "Website<br>Entry"
+                ],
+                "item": {
+                    "offset-y": -20,
+                    "font-size": "14px",
+                    
+                }
+            },
+            "tooltip": {
+                "shadow": false
+            },
+            "scale-y":{
+              "visible":false
+            },
+            "scale-y-2": {
+              "values": [
+                    "Goals",
+                    "Corner",
+                    "Fault",
+                    "Score"
+                ],
+                "guide":{
+                  "items":[
+                    {
+                      "background-color":"#fff"
+                    },
+                    {
+                      "background-color":"#eee"
+                    },
+                    {
+                      "background-color":"#ddd"
+                    },
+                    {
+                      "background-color":"#ccc"
+                    },
+                    {
+                      "background-color":"green",
+                      "alpha":0.2
+                    }
+                    ]
+                }
+            },
+            "plotarea": {
+                "margin": "75 25 50 80"
+            },
+            "plot": {
+                "tooltip-text": "%v Actions",
+                "scales": "scale-x,scale-y-2",
+                "offset": 40
+               
+            },
+            "series": [
+                {
+                    "values": [parseInt(sumascore)
+                       
+                    ],
+                    "background-color":"#5FB4E8",
+                    "border-color": "#000000",
+                    "shadow": false,
+
+                },
+                {
+                    "values": [
+                        19024
+                    ],
+                    "background-color":"#EBC765",
+                    "border-color": "#000000",
+                    "shadow": false,
+                    
+                },
+                {
+                    "values": [
+                        8965
+                    ],
+                    "background-color":"#8FB550",
+                    "border-color": "#000000",
+                    "shadow": false,
+                  
+                },
+                {
+                    "values": [
+                        parseInt(score)
+                    ],
+                    "background-color":"#D17549",
+                    "border-color": "#000000",
+                    "shadow": false
+                },
+                
+            ]
+        }
+    ]
+};
+
+zingchart.render({ 
+	id : 'myChart', 
+	data : myConfig, 
+	height: 500, 
+	width: 725 
+});
+
+ }]);
+ 
